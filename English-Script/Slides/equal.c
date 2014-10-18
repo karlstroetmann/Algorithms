68ac0c8bd41ef572df4998c97ec5f7342354ae1d
Karl Stroetmann <karl.stroetmann@dhbw-mannheim.de>
2014-10-18 22:30:29 +0200

    renamed two directories

--- English-Script/Slides/equal.c
+++
@@ -1,9 +0,0 @@
-     int stops(char* p, char* a) {
-         char* s = "int loop(char* x) { while (1) {++x;} }";
-         int e = equal(s, p, a);
-         if (e == 2) {
-             return 2;
-         } else {
-             return 1 - e;
-         }
-     }
--- Script/test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() 
-{
-	printf("%e", 7.0 - 3.0 * 34.0 / 40.0);
-}
