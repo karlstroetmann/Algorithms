af502bef3576fda43f86938a12ba50640c4a1b0f
karlstroetmann <karl.stroetmann@dhbw-mannheim.de>
2019-04-13 23:59:18 +0200

    stack implemented in Python

---
+++ Python/stack-array.py
@@ -0,0 +1,50 @@
+
+class Stack:
+    def __init__(self):
+        self.mStackElements = []
+
+    def push(self, e):
+        self.mStackElements.append(e)
+
+    def pop(self):
+        assert len(self.mStackElements) > 0, "popping empty stack"
+        self.mStackElements = self.mStackElements[:-1]
+
+    def top(self):
+        assert len(self.mStackElements) > 0, "top of empty stack"
+        return self.mStackElements[-1]
+
+    def isEmpty(self):
+        return self.mStackElements == []
+
+    def copy(self):
+        C = Stack()
+        C.mStackElements = self.mStackElements[:]
+        return C
+
+    def __str__(self):
+        C = self.copy()
+        result = C._convert()
+        dashes = "-" * len(result)
+        return '\n'.join([dashes, result, dashes])
+
+    def _convert(self):
+        if self.isEmpty():
+            return '|'
+        t = self.top()
+        self.pop()
+        return self._convert() + ' ' + str(t) + ' |'
+
+def createStack(L):
+    S = Stack()
+    n = len(L)
+    for i in range(n):
+        S.push(L[i])
+        print(S)
+    return S
+
+S = createStack(range(10))
+
+for i in range(10):
+    S.pop()
+    print(S)
