d5f1c98ab730a0c47c933487f1cab4237d86857b
karlstroetmann <karl.stroetmann@dhbw-mannheim.de>
2022-04-11 01:35:27 +0200

    section on recurrence equations extended

---
+++ Python/algo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+conda create -y -n algo python=3.9 jupyter notebook 
+conda activate algo
+conda install -c anaconda -y graphviz  
+conda install -c conda-forge -y python-graphviz ipycanvas matplotlib
