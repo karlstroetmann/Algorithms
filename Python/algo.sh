b9e02d8fcd9eff18a674f522953659bfad0b9ffe
Karl Stroetmann <karl.stroetmann@dhbw-mannheim.de>
2022-04-19 17:11:01 +0200

    typo corrected

--- Python/algo.sh
+++ Python/algo.sh
@@ -2,4 +2,4 @@
 conda create -y -n algo python=3.9 jupyter notebook 
 conda activate algo
 conda install -c anaconda -y graphviz  
-conda install -c conda-forge -y python-graphviz ipycanvas matplotlib
+conda install -c conda-forge -y python-graphviz ipycanvas matplotlib seaborn
